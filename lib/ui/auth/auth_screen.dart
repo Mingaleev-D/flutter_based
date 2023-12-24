580ba9dc5953317ea682a88962b28f935e58955b
admin <mingaleev.d92@mail.ru>
2023-12-24 08:46:09 +0300

    Initial commit

---
+++ lib/ui/auth/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('Hello'),
+          Text('Hello1'),
+        ],
+      ),
+    );
+  }
+}
